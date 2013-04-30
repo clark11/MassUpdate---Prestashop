1832cd3f77d079c0df211f5c3186a82129283b53
Claudio Casuccio <clark11@303136.no-reply.drupal.org>
2013-04-30 17:19:00 +0200

    n.e

---
+++ massupdate.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Original author: David T Baker
+ * Web: http://dtbaker.com.au
+ * Email: dtbaker@gmail.com
+ * Created: 2009-02-26
+ * 
+ * Updated by author: Bob Claassen
+ * Web: http://www.bnc-automatisering.nl
+ * Email: bc@bnc-automatisering.nl
+ * Update date: 2011-12-28
+ *
+ * File: massupdate.php
+ * Provides:
+ *  Ability to perform mass updates on products..
+ * 
+ */
+class MassUpdate extends Module
+{
+	/* @var boolean error */
+	protected $error = false;
+	private $_tabClass = 'AdminMassUpdate';
+	
+	
+	function __construct()
+	{
+	 	$this->name = 'massupdate';
+	 	$this->tab = 'administration';
+	 	$this->version = '1.6';
+
+	 	parent::__construct();
+
+	 	/* The parent construct is required for translations */
+		$this->page = basename(__FILE__, '.php');
+        $this->displayName = $this->l('Mass Update');
+        $this->description = $this->l('Perform mass updates on all your products (price, weight, and features)');
+		$this->confirmUninstall = $this->l('Aww... are you sure you want to delete me?');
+	}
+	
+	function install($try_again=true)
+	{
+	 	//if (!$this->addMassUpdateHook() || !parent::install() || !$this->registerHook('shippingCalculate') || !$this->registerHook('shippingCalculateDays')){
+	 	if (!parent::install() || 
+			!$this->installModuleTab('AdminMassUpdate', 'Mass Update', 1)){
+ 			return false;
+	 	}
+	 	
+	 	return (Configuration::updateValue('PS_MASSUPDATE_TITLE', array('1' => 'Mass Update')) );
+	}
+	
+	function uninstall()
+	{
+	 	if (!parent::uninstall() ||
+			!$this->uninstallModuleTab('AdminMassUpdate')
+			)
+	 		return false;
+	 	
+	 		
+	 	return (Configuration::deleteByName('PS_MASSUPDATE_TITLE'));
+	}
+	
+	private function installModuleTab($tabClass, $tabName, $idTabParent)
+	{
+		@copy(_PS_MODULE_DIR_.$this->name.'/logo.gif', _PS_IMG_DIR_.'t/'.$tabClass.'.gif');
+		$tab = new Tab();
+		foreach (Language::getLanguages() as $language)
+			$tab->name[$language['id_lang']] = $tabName; 
+		$tab->class_name = $tabClass;
+		$tab->module = $this->name;
+		$tab->id_parent = $idTabParent;
+		if(!$tab->save())
+		return false;
+	  return true;
+	} 
+	
+	private function uninstallModuleTab($tabClass)
+	{
+		$idTab = Tab::getIdFromClassName($tabClass);
+		if($idTab != 0){
+			$tab = new Tab($idTab);
+			$tab->delete();
+			return true;
+		}
+		return false;
+	} 
+	
+	function getContent()
+    {
+		global $cookie;
+		$tab = $this->_tabClass;
+		$token = Tools::getAdminToken($tab.(int)(Tab::getIdFromClassName($tab)).(int)($cookie->id_employee));
+		Tools::redirectAdmin('index.php?tab=' . $tab . '&token=' . $token);
+    }
+	
+	
+}
+?>
---
+++ nl.php
@@ -0,0 +1,13 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{massupdate}prestashop>adminmassupdate_dbdd82f4f2a26b92bda60075c821cf5e'] = 'Bulk bewerken ';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_db3f601e659bbdfb00fd47ebe7bafa4b'] = 'Er is een fout opgetreden tijdens het updaten, probeer nogmaals op te slaan.';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_6ebe61f77591fbab1061551e24a841a3'] = 'De producten zijn succesvol bijgewerkt!';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_5cd24900fb6828872abb0664e992b010'] = 'Bewerk meerdere product tegelijk';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_34517a5c64f3f728c44dbd906b097bd1'] = 'Alle producten opslaan';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_962c47536c63765c0752dda2cdffffdb'] = 'Bijwerken.......Even geduld a.u.b......';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_3601146c4e948c32b6424d2c0a7f0118'] = 'Prijs';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_8c489d0946f66d17d73f26366a4bf620'] = 'Gewicht';
+$_MODULE['<{massupdate}prestashop>adminmassupdate_694e8d1f2ee056f98ee488bdc4982d73'] = 'Hoeveelheid';
